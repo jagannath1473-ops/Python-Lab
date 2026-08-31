1a556b1ced86a2fa40e8b5e118ef0985108d11d6
jagannath1473-ops <jagannath1473@gmail.com>
2026-08-31 15:14:39 +0530

    Create fibo.py

---
+++ fibo.py
@@ -0,0 +1,6 @@
+def fibonacci(n):
+if n == 0 or n == 1:
+return n return fibonacci(n - 1) + fibonacci(n - 2)
+
+n = int(input(&quot;Enter number of terms: &quot;))
+for i in range(n): print(fibonacci(i), end=&quot; &quot;)
